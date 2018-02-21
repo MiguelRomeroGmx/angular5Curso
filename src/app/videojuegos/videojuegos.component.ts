caec77b1021a4f8ad7db2114e9243cdeb7561187
MiguelRomeroGmx <contacto@miguelromero.mx>
2018-02-21 14:14:17 -0600

    Condicional ngIf

--- src/app/videojuegos/videojuegos.component.ts
+++ src/app/videojuegos/videojuegos.component.ts
@@ -10,6 +10,9 @@ export class VideojuegosComponent implements OnInit {
 
   public nombre = "Video Juegos 2018";
   public mejor_juego = "GTA 5";
+  public mejor_juego_retro = "Super Mario 64";
+  public mostrar_retro = true;
+  public color = "orange";
   constructor() { }
 
   ngOnInit() {
